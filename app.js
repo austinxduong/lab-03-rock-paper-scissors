44f632a62ee05c44d63a94d3875a71341a6e95b1
austinxduong <axduong2@gmail.com>
2021-03-26 00:29:39 -0700

    tested TDD

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 // import functions and grab DOM elements
+import { getComputerThrow } from './utils.js';
+import { didUserWin } from './utilis.js';
+
+const playButton = document.getElementById('play-button');
+const resetButton = document.getElementById('reset-button');
+const winResults = document.getElementById('wins');
+const loseResults = document.getElementById('lose');
+const drawResults = document.getElementById('draws');
+const totalResults = document.getElementById('total');
+const results = document.getElementById('results');
+
+//makesure to console. these tomorrow
+
+console.log(playButton, resetButton, winResults, loseResults, drawResults, totalResults, results)
 
 // initialize state
+let wins = 0;
+let lose = 0;
+let draws = 0;
+let total = wins + lose + draws;
+
 
 // set event listeners to update state and DOM
+playButton.addEventListener('click', () => {
+    const alexaNumber = Math.ceil(Math.random()) * 3;
+    const alexaValue = getComputerThrow (alexaNumber)
+
+
+
+}
--- utils.js
+++ utils.js
@@ -1,19 +1,32 @@
-export function getComputerThrow(randomNumber) {
-        let randomNumber = Math.ceil(Math.random()) *3;
-        let handShape
-        if (randomNumber === 1) {
-            handShape = 'rock'
+export function getComputerThrow() {
+    let randomNumber = Math.ceil(Math.random()) * 3;
+    let alexa = '';
+    if (randomNumber === 1) {
+        alexa = 'rock';
     }
-    if(randomNumber === 2) {
-            handShape = 'paper'
+    if (randomNumber === 2) {
+        alexa = 'paper';
     }
     if (randomNumber === 3) {
-        handShape = 'scissors'
+        alexa = 'scissors';
     }
-    return handShape;
+
 }
 
 
-export function didUserWin(play, computer) {
-    if player === 'rock' && computer === 'paper') {
-        return 'lose';
+export function didUserWin(player, alexa) {
+    if (player === 'rock' && alexa === 'paper'){
+        return 'lose';}
+    else if (player === 'rock' && alexa === 'scissors'){
+        return 'win';}
+    else if (player === 'paper' && alexa === 'rock'){
+        return 'win';}
+    else if (player === 'paper' && alexa === 'scissors'){
+        return 'lose';}
+    else if (player === 'scissors' && alexa === 'rock'){
+        return 'lose';}
+    else if (player === 'scissors' && alexa === 'paper'){
+        return 'win';}
+    else {return 'draw';}
+
+}
